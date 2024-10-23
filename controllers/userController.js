0adea0dae161eae5968d7a2077814432b3a5da43
Satish Singh <satishsingh12@gmail.com>
2024-10-23 15:34:21 +0530

    add - reset password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const Users = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 const userController = {
   searchUser: async (req, res) => {
@@ -45,6 +46,29 @@ const userController = {
       return res.status(201).json({ message: "Updated successfully!" });
     } catch (err) {
       return res.status(500).json({ message: err.message });
+    }
+  },
+
+  resetPassword: async (req, res) => {
+    try {
+      const { currentPassword, newPassword } = req.body;
+      const user = await Users.findById({ _id: req.params.id });
+
+      const isMatch = await bcrypt.compare(currentPassword, user.password);
+      if (!isMatch)
+        return res
+          .status(400)
+          .json({ message: "Current password is incorrect" });
+
+      const salt = await bcrypt.genSalt(10);
+      const hashPassword = await bcrypt.hash(newPassword, salt);
+
+      user.password = hashPassword;
+      await user.save();
+
+      res.status(200).json({ message: "Password updated successfully" });
+    } catch (err) {
+      return res.status(500).json({ message: "Server error" });
     }
   },
 
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,6 +5,7 @@ const userController = require("../controllers/userController");
 router.get("/search", auth, userController.searchUser);
 router.get("/user/:id", auth, userController.getUser);
 router.patch("/user/:id", auth, userController.updateUser);
+router.patch("/user/:id/reset_password", auth, userController.resetPassword);
 router.patch("/user/:id/friend", auth, userController.friend);
 router.patch("/user/:id/unfriend", auth, userController.unfriend);
 
