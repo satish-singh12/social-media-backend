c9e66ac91d426e4d03701bcaa9dc13f6c41de8cd
Satish Singh <satishsingh12@gmail.com>
2024-10-31 11:09:19 +0530

    correction done in logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -101,7 +101,7 @@ const authController = {
   logout: async (req, res) => {
     try {
       res.clearCookie("refreshToken", { path: "api/refresh_token" });
-      res.json({ message: "Logged out successfully" });
+      res.status(200).json({ message: "Logged out successfully" });
     } catch (err) {
       res.status(500).json({ message: err.message });
     }
